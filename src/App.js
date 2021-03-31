8142c6e29b4dc3f4d159190dac0b32c878867e34
Yohenba18 <roddik12kshetri@gmail.com>
2021-03-31 21:33:06 +0530

    network

--- src/App.js
+++ src/App.js
@@ -5,16 +5,27 @@ import './App.css';
 
 class App extends Component{
 
-async loadBlockchainDate(){
+  componentWillMount() {
+    this.loadBlockchainData();
+  }
+
+async loadBlockchainData(){
   const web3 = new Web3(Web3.givenProvider || "http://localhost:8545");
   const network =await web3.eth.net.getNetworkType()
-  console.log("network: ",network)
+  const accounts  = await web3.eth.getAccounts()
+  console.log("account: " , accounts[0])
+}
+
+constructor(props){
+  super(props);
+  this.state = {account : ' '}
 }
 
   render()  {
     return (
     <div className="container">
       <h1>Hello World</h1>
+      <p>Your Acoount: </p>
     </div>
   );
   }
